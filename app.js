594e067c7e4c7d82ee293a867d87d44c2a3dc738
Jhordan Jhovany Laureano Meza <jhordan.laureano@unmsm.edu.pe>
2025-01-31 11:31:20 -0500

    probando cambios en github pages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let numeroSecreto = 0;
 let intentos = 0;
 let listaNumerosSorteados = [];
-let numeroMaximo = 10;
+let numeroMaximo = 100;
 
 function asignarTextoElemento(elementoHTML,texto){
     let elemento = document.querySelector(elementoHTML);
